39fd85294c1dca9262f98642f5a0124dcc9aa771
Elvis Makia <elvis_makia@yahoo.com>
2024-07-09 17:06:41 -0400

    convert clss to func implement useState  commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Layout/Header";
 import Footer from "./Components/Layout/Footer";
 import CyclOPediaCLassPage from "./Components/Layout/CyclOPediaCLassPage";
+import CyclOPediaCLassFuncPage from "./Components/Layout/CyclOPediaCLassFuncPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,6 +13,10 @@ root.render(
       <div className="col-6">
         <span className="h1 text-warning text-center">Class Component</span>
         <CyclOPediaCLassPage />
+      </div>
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Class FuncComponent</span>
+        <CyclOPediaCLassFuncPage />
       </div>
     </div>
     <Footer />
